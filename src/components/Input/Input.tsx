56bb0f7b349a67d48f1cd912b2443c8a23b92b08
adam <adamsalej@gmail.com>
2023-07-30 22:43:13 +0200

    Remove attribute for Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,12 +3,5 @@ import { IInput } from './types';
 import { StyledInput } from './style';
 
 export const Input: FC<IInput> = ({ pin, inputRef }) => (
-	<StyledInput
-		type='text'
-		minLength={4}
-		maxLength={4}
-		value={pin.hashedValue}
-		ref={inputRef}
-		disabled
-	/>
+	<StyledInput type='text' value={pin.hashedValue} ref={inputRef} disabled />
 );
